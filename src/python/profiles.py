7f709c4a101b111377c7dd953646e4eb2bdfb12c
QEDan <danpmazur@gmail.com>
2026-04-27 14:28:52 -0700

    implemented profiles for step-profile tube

--- src/python/profiles.py
+++ src/python/profiles.py
@@ -18,29 +18,58 @@ class FieldProfile:
         return self.rho, self.a_phi, self.da_phi
 
 class StepFunctionProfile(FieldProfile):
-    def __init__(self, rho, lambd, F, e=1.0):
+    def __init__(self, rho, lambd, F, e=1.0, smooth_width=None):
+        """
+        Magnetic field B = (F / (pi * lambd^2)) * theta(lambd - rho)
+        A_phi = (F / (2*pi)) * f_lambda(rho) / rho
+        f_lambda(rho) = (rho^2/lambd^2) * theta(lambd - rho) + theta(rho - lambd)
+        """
         super().__init__(rho)
         self.lambd = lambd
         self.F = F
         self.e = e
+        self.smooth_width = smooth_width
         self.update()
 
     def update(self):
-        # Aphi = F/(2*pi) * (rho/lambd^2 if rho < lambd else 1/rho)
-        inner = self.rho < self.lambd
-        outer = ~inner
+        pre = self.F / (2.0 * np.pi)
         
-        pre = self.F / (2 * np.pi)
-        
-        # Initialize tensors if not already
-        self.a_phi = torch.zeros_like(self.rho)
-        self.da_phi = torch.zeros_like(self.rho)
-        
-        # Fill a_phi
-        self.a_phi = torch.where(inner, pre * self.rho / (self.lambd**2), pre / self.rho)
-        
-        # Fill da_phi
-        self.da_phi = torch.where(inner, pre / (self.lambd**2), -pre / (self.rho**2))
+        if self.smooth_width is None:
+            # Sharp step function
+            inner = self.rho < self.lambd
+            
+            # f_lambda(rho)
+            f_lambd = torch.where(inner, (self.rho**2) / (self.lambd**2), torch.ones_like(self.rho))
+            
+            # A_phi = pre * f_lambda / rho
+            self.a_phi = pre * f_lambd / self.rho
+            
+            # B = pre * 2 / lambd^2 for rho < lambd, else 0
+            b_field = torch.where(inner, 2.0 * pre / (self.lambd**2), torch.zeros_like(self.rho))
+            
+            # da_phi = B - a_phi / rho
+            self.da_phi = b_field - self.a_phi / self.rho
+        else:
+            # Smoothed step function using sigmoid for theta(lambd - rho)
+            # theta(x) approx 0.5 * (1 + tanh(x / smooth_width))
+            # x = lambd - rho
+            arg = (self.lambd - self.rho) / self.smooth_width
+            theta_smooth = 0.5 * (1.0 + torch.tanh(arg))
+            
+            # f_lambda_smooth = (rho^2/lambd^2) * theta_smooth + (1 - theta_smooth)
+            f_lambd = (self.rho**2 / self.lambd**2) * theta_smooth + (1.0 - theta_smooth)
+            
+            self.a_phi = pre * f_lambd / self.rho
+            
+            # Derivative of f_lambda
+            # d/drho theta_smooth = 0.5 * (1 - tanh^2(arg)) * (-1/smooth_width)
+            d_theta = -0.5 * (1.0 - torch.tanh(arg)**2) / self.smooth_width
+            
+            df_lambd = (2.0 * self.rho / self.lambd**2) * theta_smooth + \
+                       (self.rho**2 / self.lambd**2) * d_theta - d_theta
+            
+            # da_phi = pre * (df_lambd / rho - f_lambd / rho^2)
+            self.da_phi = pre * (df_lambd / self.rho - f_lambd / (self.rho**2))
 
 class DifferentiableProfile(FieldProfile):
     """
